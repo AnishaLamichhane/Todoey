a9b28ab1bae9000985c38b300164e5942fa4594e
AnishaLamichhane <ansalamichhane111@gmail.com>
2021-10-12 09:09:02 +0545

    table view delegate methods

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -8,11 +8,9 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-let itemArray = ["Find mike", "nobody", "Maid"]
+    let itemArray = ["Find mike", "nobody", "Maid"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.isNavigationBarHidden = false
-
     }
     
     //MARK: - TableView Datasource Methods
@@ -21,18 +19,28 @@ let itemArray = ["Find mike", "nobody", "Maid"]
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
-
+    
     // Provide a cell object for each row.
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-       // Fetch a cell of the appropriate type.
-       let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-       
-       // Configure the cell’s contents.
+        // Fetch a cell of the appropriate type.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        // Configure the cell’s contents.
         cell.textLabel?.text = itemArray[indexPath.row]
-           
-       return cell
+        
+        return cell
     }
-
-
+    
+    //MARK: - TableView Delegate Methods
+//    these methods are fired whenever the tableview cells are clicked
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(itemArray[indexPath.row])
+        if (tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark){
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
